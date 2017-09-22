b29aae2a1be551ccc0d2f973e9dbd4312e4babe7
HA$HT@G <hasherror404@gmail.com>
2017-09-22 20:30:58 +0530

    boardconfig:change kernel config
    Signed-off-by: HA$HT@G <hasherror404@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogen_core33g_defconfig
+TARGET_KERNEL_CONFIG := lineage_core33g_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/core33g
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/core33g/dt.img
 
