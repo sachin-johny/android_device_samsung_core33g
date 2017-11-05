6d047bc930c2b0aff0a764d9826a8e25ef635e17
hash07 <hasherror404@gmail.com>
2017-11-05 15:17:20 +0530

    omni_core33g.mk: fix omni stuffs inheritance
    Change-Id: If6969747b86566a89bfd287e7477f7858350f635
    Signed-off-by: hash07 <hasherror404@gmail.com>

--- omni_core33g.mk
+++ omni_core33g.mk
@@ -4,8 +4,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Release name
 PRODUCT_RELEASE_NAME := core33g
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/omni/common.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 
 ## Device identifier. This must come after all inclusions
