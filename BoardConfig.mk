diepquynh/android_device_samsung_grandprimeve3g

ril: Add open-source RIL
Taken from TeamNexus' zero-common

Change-Id: I4cb23fc9f071e4e25f1fd69517548a4810c20da2
Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

BoardConfig.mk
@@ -163,3 +163,7 @@ TARGET_SCREEN_WIDTH := 540
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SM-G531H,SM-G531BT,grandprimeve3g,grandprimeve3gdtv,grandprimeve3gub,grandprimeve3gxx,grandprimeve3gdtvvj
+
+# RIL
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/grandprimeve3g/include
+SIM_COUNT := 2

sachin-johny/android_device_samsung_core33g

BoardConfig.mk
@@ -62,3 +62,6 @@ TARGET_SCREEN_WIDTH := 480
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SM-G360H,SM-G360HU,core33g,core33gdd,core33gdx
 
+# RIL
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/core33g/include
+SIM_COUNT := 2